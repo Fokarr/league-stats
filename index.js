36df4c2526f06b7667f4632fb35bcaa82994aaad
Fokarr <jarmof135@gmail.com>
2020-12-18 01:11:46 +0100

    get MatchHistory and get SummonerByName added

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const apiKey = process.env.RIOT_API_KEY
+
+const getSummonerByName = async name => {
+    try {
+        const response = await axios.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + apiKey)
+        return response
+    } 
+    catch(e) {
+        return e
+    }
+}
+
+const getMatchHistory = async accountId => {
+    try {
+        const response = await axios.get("https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?api_key=" + apiKey)
+        return response
+    }
+    catch(e) {
+        return e
+    }
+}
+
+const users = ["MSPaint%20Picasso", "Volker%20Racho", "GingerAone"]
+
+
+users.forEach(user => {
+    getSummonerByName(user).then((res) => {
+
+        const account = {
+            id: res.data.id,
+            accountId: res.data.accountId,
+            puuid: res.data.puuid,
+            name: res.data.name,
+        }
+    
+        getMatchHistory(account.accountId).then((res2) => {
+            console.log(res2.data)
+        })
+    
+    })
+})
+
+/*getMatchHistory("7ExI8YIXh7Y1IWisDvjiYZgXfFo7S2HM5wQKOFaTJ3PDUA").then((res2) => {
+    console.log(res2.data)
+})
+
+getSummonerByName("MSPaint%20Picasso").then((res) => {
+    console.log(res.data)
+})*/
